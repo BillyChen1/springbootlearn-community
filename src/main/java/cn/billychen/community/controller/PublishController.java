e90aa89469798490b4c339fda8da3a720639b59b
BillyChen <whqscqm@outlook.com>
2020-06-26 20:56:59 +0800

    add comment

---
+++ src/main/java/cn/billychen/community/controller/PublishController.java
@@ -0,0 +1,69 @@
+package cn.billychen.community.controller;
+
+import cn.billychen.community.mapper.QuestionMapper;
+import model.Question;
+import model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class PublishController {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @GetMapping("/publish")
+    public String publish() {
+        return "publish";
+    }
+
+    //发布问题，将问题上传到服务端数据库
+    @PostMapping("/publish")
+    public String doPublish(@RequestParam("title") String title,
+                            @RequestParam("description") String description,
+                            @RequestParam("tag") String tag,
+                            HttpServletRequest request,
+                            Model model) {
+        if (title == null || title == "") {
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+        if (description == null || description == "") {
+            model.addAttribute("error", "问题补充不能为空");
+            return "publish";
+        }
+        if (tag == null || tag == "") {
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+
+
+        Question question = new Question();
+        question.setTitle(title);
+        question.setDescription(description);
+        question.setTag(tag);
+
+        //获取User
+        User user = (User) request.getSession().getAttribute("user");
+
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtModified(question.getGmtCreate());
+
+        questionMapper.create(question);
+        //发帖成功重定向回到首页
+        return "redirect:/";
+    }
+}
